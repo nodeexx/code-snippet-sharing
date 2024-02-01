b63580e5780a1074fa8ef4e5d9b6ea9a78052af4
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-02-01 22:41:06 +0100

    Add Sentry variables to server and client configs

--- src/lib/client/core/config/index.ts
+++ src/lib/client/core/config/index.ts
@@ -1,6 +1,15 @@
+import {
+  PUBLIC_SENTRY_ENVIRONMENT,
+  PUBLIC_SENTRY_DSN,
+} from '$env/static/public';
+
 export const config = {
   appName: 'Code Snippet Sharing app',
   get pageTitleSuffix() {
     return ` | ${this.appName}`;
+  },
+  sentry: {
+    dsn: PUBLIC_SENTRY_DSN,
+    environment: PUBLIC_SENTRY_ENVIRONMENT,
   },
 };
