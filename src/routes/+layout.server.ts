3baf7d79ce895e373f524503f03e606af0d89a87
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-02-04 21:54:04 +0100

    Fix typing in +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
-import type { LayoutServerLoad } from './$types';
 import { loadFlash } from 'sveltekit-flash-message/server';
 
-export const load: LayoutServerLoad = loadFlash(async ({ locals }) => {
+export const load = loadFlash(async ({ locals }) => {
   // NOTE: Used only for the sibling `+error.svelte` that calls
   // `invalidateAll`. ALWAYS add this auth block to each nested page that
   // is accessible to unauthenticated users, but may conditionally display
