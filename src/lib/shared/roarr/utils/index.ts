9b62a117404631c0eb276faf2f6200469b0db758
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-03-11 20:42:22 +0100

    Create an util log function to log an error with Roarr and send it to Sentry

---
+++ src/lib/shared/roarr/utils/index.ts
@@ -0,0 +1,8 @@
+import { sentry } from '$lib/shared/sentry';
+import type { SeverityLevel } from '@sentry/sveltekit';
+import { roarr } from '../client';
+
+export function logError(error: Error, level: SeverityLevel = 'error'): void {
+  roarr.error(error.message, { error }, 4);
+  sentry?.captureException(error, { level });
+}
