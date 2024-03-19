a834d184d97d1b8c002826c68378a410205acada
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-03-19 08:49:28 +0100

    Setup Sentry performance monitoring for DB queries

--- src/lib/server/sentry/utils/index.ts
+++ src/lib/server/sentry/utils/index.ts
@@ -1,12 +1,20 @@
 import { posthog, PostHogSentryIntegration } from '$lib/server/posthog';
 import type { Integration } from '@sentry/types';
+import * as Sentry from '@sentry/sveltekit';
+import { prisma } from '$lib/server/prisma';
 
 export function getServerSentryIntegrations(
   organization: string | undefined,
 ): Integration[] {
+  const integrations: Integration[] = [
+    new Sentry.Integrations.Prisma({ client: prisma }),
+  ];
+
   if (posthog && organization) {
-    return [new PostHogSentryIntegration(posthog, undefined, organization)];
+    integrations.unshift(
+      new PostHogSentryIntegration(posthog, undefined, organization),
+    );
   }
 
-  return [];
+  return integrations;
 }
